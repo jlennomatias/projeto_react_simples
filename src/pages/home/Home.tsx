566e5bfa1f2858786555ab55aad065febdb1fc77
jlennomatias <john.x12@hotmail.com>
2024-01-17 11:05:13 -0300

    incluindo rotas e paginas de login e home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import { Container, Grid } from '@mui/material'
-import ButtonAppBar from '../components/Navbar'
-import PokeCard from '../components/PokemonCard'
+import ButtonAppBar from '../../components/Navbar'
+import PokeCard from '../../components/PokemonCard'
 
 export const Home = () => {
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes as Switch, BrowserRouter, Navigate } from "react-router-dom"
+import {Login, Home} from "../pages"
 
 export const Routes = () => {
 
@@ -6,9 +7,10 @@ export const Routes = () => {
         <BrowserRouter>
             <Switch>
                 {/* <Route path="/" Component={() => <h1>Ola mundo</h1>}/> */}
-                <Route path="/" element={<Navigate to="/pagina-inicial" />}/>
+                <Route path="/login" Component={Login}/>
+                <Route path="/home" Component={Home}/>
 
-                <Route path="*" element={<Navigate to="/pagina-inicial" />}/>
+                <Route path="*" element={<Navigate to="/home" />}/>
             </Switch>
         </BrowserRouter>
     )
